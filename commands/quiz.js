0e948e3313a164518cd59b19ef443aac58a9d514
ggpixel <81717063+ggpix@users.noreply.github.com>
2023-03-06 06:13:41 +0100

    Update quiz.js

--- commands/quiz.js
+++ commands/quiz.js
@@ -1,7 +1,45 @@
 const { EmbedBuilder, SlashCommandBuilder } = require("discord.js")
 const noblox = require('noblox.js')
 
-var questions = JSON.parse(process.env.questions)
+const dev_key = process.env.dev_key";
+const login_url = "https://pastebin.com/api/api_login.php";
+const paste_url = "https://pastebin.com/api/api_post.php";
+
+const username = process.env.username; // replace with your Pastebin account username
+const password = process.env.password; // replace with your Pastebin account password
+
+var questions
+
+// log in to Pastebin and generate user API key
+fetch(login_url, {
+  method: "POST",
+  headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  body: `api_dev_key=${dev_key}&api_user_name=${username}&api_user_password=${password}`,
+})
+  .then((response) => response.text())
+  .then((data) => {
+    const user_key = data; // store the user API key in a variable
+
+    // retrieve the private paste
+    const paste_key = "NaRuZU5i"; // replace with the key of the paste you want to retrieve
+    const option = "show_paste";
+    const body = `api_dev_key=${dev_key}&api_user_key=${user_key}&api_option=${option}&api_paste_key=${paste_key}&api_password=${process.env.paste_pass}`;
+
+    fetch(paste_url, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: body,
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        questions = data // print the content of the paste
+      })
+      .catch((error) => console.log(error));
+  })
+  .catch((error) => console.log(error));
+
+
+questions = JSON.parse(questions)
 
 module.exports = {
   cooldown: 10,
